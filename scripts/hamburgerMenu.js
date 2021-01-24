04340dab0a4396204875c048d1667060173dfdc1
JuanRassa <jmrassaproduccion@gmail.com>
2021-01-24 12:53:13 -0500

    Adding hamburger nav functionality for mobile.

---
+++ scripts/hamburgerMenu.js
@@ -0,0 +1,23 @@
+/** 
+ * @function displayHideNavList
+ * @
+ * 
+*/
+let userNavToggleView = document.getElementById('hamburgerMenu');
+let navDisplay = document.getElementById('navUl')
+let hamburgerIconNav = document.getElementsByClassName('fa-bars')[0];
+let closeIconNav = document.getElementsByClassName('fa-times')[0];
+const displayHideNavList = () => {
+  navDisplay.classList.toggle('flexContainer');
+  if (hamburgerIconNav.style.display === '' || hamburgerIconNav.style.display === 'block') {
+    hamburgerIconNav.style.display = 'none';
+  } else if (hamburgerIconNav.style.display === 'none')  {
+    hamburgerIconNav.style.display = 'block';
+  }
+  if (closeIconNav.style.display === '' || closeIconNav.style.display === 'none') {
+    closeIconNav.style.display = 'block';
+  } else if (closeIconNav.style.display === 'block')  {
+    closeIconNav.style.display = 'none';
+  }
+}
+userNavToggleView.addEventListener('change', displayHideNavList)
