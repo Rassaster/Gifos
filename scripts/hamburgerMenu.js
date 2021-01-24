69ed82897676074006c0fec98b7d10ea648c8a4b
JuanRassa <jmrassaproduccion@gmail.com>
2021-01-24 16:26:33 -0500

    Changing hamburger menu method. Adding svg.

--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -8,16 +8,7 @@ let navDisplay = document.getElementById('navUl')
 let hamburgerIconNav = document.getElementsByClassName('fa-bars')[0];
 let closeIconNav = document.getElementsByClassName('fa-times')[0];
 const displayHideNavList = () => {
-  navDisplay.classList.toggle('flexContainer');
-  if (hamburgerIconNav.style.display === '' || hamburgerIconNav.style.display === 'block') {
-    hamburgerIconNav.style.display = 'none';
-  } else if (hamburgerIconNav.style.display === 'none')  {
-    hamburgerIconNav.style.display = 'block';
-  }
-  if (closeIconNav.style.display === '' || closeIconNav.style.display === 'none') {
-    closeIconNav.style.display = 'block';
-  } else if (closeIconNav.style.display === 'block')  {
-    closeIconNav.style.display = 'none';
-  }
+  navDisplay.classList.toggle('showNav');
+  userNavToggleView.classList.toggle('showClose')
 }
-userNavToggleView.addEventListener('change', displayHideNavList);
+userNavToggleView.addEventListener('click', displayHideNavList);
