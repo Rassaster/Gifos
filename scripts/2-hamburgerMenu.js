ae9279f7310695d9afc541869ac9b1cea39156dd
JuanRassa <jmrassaproduccion@gmail.com>
2021-01-24 19:58:13 -0500

    Removing unnecessary variable. Script ready.

--- scripts/2-hamburgerMenu.js
+++ scripts/2-hamburgerMenu.js
@@ -1,14 +1,12 @@
 /** 
  * @function displayHideNavList
- * @
- * 
-*/
+ * @desc For Tablet/Mobile screen, toggles the view of Nav Menu, as well as its UIs icons.
+ */
 let userNavToggleView = document.getElementById('hamburgerMenu');
 let navDisplay = document.getElementById('navUl')
-let hamburgerIconNav = document.getElementsByClassName('fa-bars')[0];
-let closeIconNav = document.getElementsByClassName('fa-times')[0];
 const displayHideNavList = () => {
   navDisplay.classList.toggle('showNav');
   userNavToggleView.classList.toggle('showClose')
 }
 userNavToggleView.addEventListener('click', displayHideNavList);
+console.log(hello)
