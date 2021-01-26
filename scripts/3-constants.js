89f8318154efff273430398e6463b843fa94a24a
JuanRassa <jmrassaproduccion@gmail.com>
2021-01-25 23:31:15 -0500

    Adding Search Gifs functio. (Fetch and append to DOM)

--- scripts/3-constants.js
+++ scripts/3-constants.js
@@ -7,6 +7,9 @@
  * @const Giphy_Trending_Search_Terms_Node
  * @const Giphy_Search_Query
  * @const Giphy_Trending_Limit
+ * @const userSearchInput
+ * @const searchButton
+ * @const arraySearchGifsResults
  * @const arrayTrendingGifsResults
  */
 const Giphy_BaseURL = "https://api.giphy.com/v1/";
@@ -14,20 +17,37 @@ const Giphy_APIKey = "?api_key=33l2FVbyT45wmg6e3MJf38JvhgOSNzdH";
 const Giphy_Search_Node = 'gifs/search';
 const Giphy_Trending_Search_Terms_Node = 'trending/searches'
 const Giphy_Trending_Node = "gifs/trending";
-// const Giphy_Search_Query = TBC;
+const Giphy_Search_Query = `&q=`;
 const Giphy_Results_Limit = '&limit=10';
+const userSearchInput = document.getElementById('searchBar');
+const searchButton = document.getElementById('searchButton-searchBar');
+const arraySearchGifsResults = [];
 const arrayTrendingGifsResults = [];
 /**
  * @async
- * @function Declaration-gifsRequest()
- * @param Yes 3(giphyNode, searchQuery, resultsLimit) 
+ * @function gifsRequest()
+ * @param {giphyNode, searchQuery, resultsLimit}
  * @description Launches a request to Giphy's Trending Gifs end point.
- * @returns An array containing the Trending Gifs JSON data received in the request.
+ * @returns {array} An array containing the Trending Gifs JSON data received in the request.
  */
-const gifsRequest = async (giphyNode, searchQuery = '', resultsLimit = '',) => { 
-  let response = await fetch(Giphy_BaseURL + giphyNode + Giphy_APIKey + searchQuery + resultsLimit);
+const gifsTrendingsRequest = async (giphyNode, searchQuery = '', resultsLimit = '',) => { 
+  let response = await fetch(Giphy_BaseURL + giphyNode + Giphy_APIKey + searchQuery +  resultsLimit);
   let gifObject = await response.json();
   console.log(`${giphyNode} -> ${searchQuery} -> ${resultsLimit}:`);
+  console.table(gifObject.data);
+  return gifObject.data;
+}
+/**
+ * @async
+ * @function gifsSearchRequest()
+ * @param {giphyNode, searchQuery, resultsLimit}
+ * @description Launches a request to Giphy's Search Gifs end point. It listens to the usearSearchInput.value to define the query value.
+ * @returns {array} An array containing the Trending Gifs JSON data received in the request.
+ */
+const gifsSearchRequest = async (giphyNode, searchQuery = '', resultsLimit = '',) => { 
+  let response = await fetch(Giphy_BaseURL + giphyNode + Giphy_APIKey + searchQuery + userSearchInput.value + resultsLimit);
+  let gifObject = await response.json();
+  console.log(`${giphyNode} -> User searched: ${userSearchInput.value} -> ${resultsLimit}:`);
   console.table(gifObject.data);
   return gifObject.data;
 }
