364e849881bd7c02b099c37c8141373b820c2be7
JuanRassa <jmrassaproduccion@gmail.com>
2021-02-24 09:22:44 -0500

    Changing cameraAssets img src in toggleDarkMode.

--- scripts/1-darkMode_Toggle.js
+++ scripts/1-darkMode_Toggle.js
@@ -1,3 +1,7 @@
 toggleDarkMode.addEventListener('click', () => {
   toggleClass(webBody, 'darkMode');
+  cameraRollSmallAsset.src = 'assets/3.createGifosAssets/0.cameraAssets/elementDM-cameraRoll2-darkMode.svg';
+  cameraRollBigAsset.src = 'assets/3.createGifosAssets/0.cameraAssets/elementDM-cameraRoll3-darkMode.svg';
+  // cameraAloneAsset.src = 'assets/3.createGifosAssets/0.cameraAssets/';
+  cameraRollFloorAsset.src = 'assets/3.createGifosAssets/0.cameraAssets/elementDM-cameraRoll-darkMode.svg';
 })
